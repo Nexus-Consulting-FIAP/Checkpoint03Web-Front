08a3c06609c641fd1011dc59d43b1c0a6c8a95a9
Daniel <danieloliveiradesouza09@outlook.com>
2025-05-25 13:37:30 -0300

    contato: aviso enviado com sucesso feito

---
+++ src/js/contato.js
@@ -0,0 +1,5 @@
+const submit_result = document.getElementById("submit_result")
+const btn_enviar = document.getElementById("btn_enviar")
+btn_enviar.addEventListener("click", ()=>{
+    submit_result.textContent = "Enviado com sucesso!"
+})
